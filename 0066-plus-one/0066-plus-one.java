d0b09463d47de9db663021120fef0ab2dd20c9b9
srijan Dubey <srijandubey07@gmail.com>
2026-07-20 23:44:33 +0530

    Time: 0 ms (100%), Space: 43.4 MB (62.05%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        // Traverse from the last digit
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            // If digit is less than 9, simply add 1
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            // If digit is 9, make it 0 and continue
+            digits[i] = 0;
+        }
+
+        // If all digits were 9, create a new array
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+}
