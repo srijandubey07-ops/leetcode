26adfc861a0eaa708b91f4d1d42a9034ca10be21
srijan Dubey <srijandubey07@gmail.com>
2026-07-26 22:47:37 +0530

    Time: 1 ms (97.43%), Space: 42.3 MB (95.78%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,24 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1) {
-            return x;
-        }
-
-        int low = 1;
-        int high = x;
+        int s =1 ;
+        int e = x;
         int ans = 0;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (mid <= x / mid) {   // mid * mid <= x (avoids overflow)
+        if(x==0){
+            return 0;
+        }
+        while(s<=e){
+            int mid = (s+(e-s)/2);
+            if(mid == x/mid){
+                return mid;
+            }
+            else if (mid>x/mid){
+                // move to left
+                e = mid -1;
+            }
+            else {
                 ans = mid;
-                low = mid + 1;      // Try to find a larger valid square root
-            } else {
-                high = mid - 1;
+                s = mid+1;
             }
         }
-
         return ans;
     }
 }
