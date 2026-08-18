062c492978750698443e98c8d09b94451643a0a4
srijan Dubey <srijandubey07@gmail.com>
2026-08-18 22:11:27 +0530

    Time: 5 ms (98.74%), Space: 43.8 MB (59.18%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        char [] a = new char[128]; // '\0'
+        for(int i=0;i<s.length(); i++){
+            char ch = s.charAt(i);
+            char dh = t.charAt(i);
+            int idx = (int)ch;
+            if(a[idx]=='\0') a[idx]=dh;
+            else {
+                if(a[idx]!= dh) return false;
+            }
+        }
+        for(int i =0; i<128 ; i++){
+            a[i] = '\0';
+        }
+           for(int i=0;i<t.length(); i++){
+            char ch = t.charAt(i);
+            char dh = s.charAt(i);
+            int idx = (int)ch;
+            if(a[idx]=='\0') a[idx]=dh;
+            else {
+                if(a[idx]!= dh) return false;
+            }
+        }
+
+        return true;
+    }
+}
